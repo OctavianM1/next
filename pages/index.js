208b83659664504ea8c7b894fc888ae8f497f584
Octavian Mitu <octavian.mitu@amdaris.com>
2021-05-01 18:41:01 +0300

    change

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>TEST</main>
+      <main className={styles.main}>TEST1</main>
 
       <footer className={styles.footer}>
         <a
